17eaf5c2a51a14e0f19986bf3619ea2910c841f2
Gonzalo Romero <gondragon13@gmail.com>
2017-08-12 19:22:10 -0300

    Tabs are managed via LUA script

--- Bookstore/objects/bookstore/bookstore.lua
+++ Bookstore/objects/bookstore/bookstore.lua
@@ -11,6 +11,68 @@ function onInteraction(args)
   local storeInventory = config.getParameter("storeInventory")
   
   for genre,objects in pairs(storeInventory) do addRecipes(interactData, objects, genre) end
+  
+  --
+  -- Test things
+  --
+  local tabs = interactData["modTab"]
+  local posY = 277
+  local categories = {
+      type = "radioGroup",
+      toggleMode = false,
+      buttons = {}
+	  }
+  
+  if #tabs > 0 then
+	  for i,modtab in ipairs(tabs) do
+		local tabIcon = modtab["file"]
+		local tabName = modtab["label"]
+		local tabFilter = modtab["filter"]
+		
+		local tabIconLabel = {
+		  type = "image",
+		  file = tabIcon,
+		  position = {
+			8,
+			posY
+		  },
+		  zlevel = 3
+		}
+		
+		local tabNameLabel = {
+		  type = "label",
+		  value = tabName,
+		  position = {
+			24,
+			posY+2
+		  },
+		  zlevel = 3
+		}
+		
+		local tabButton = {
+			  selected = true,
+			  position = {
+				3,
+				posY-2
+			  },
+			  baseImage = "/interface/bookstore/unselectedTab.png",
+			  baseImageChecked = "/interface/bookstore/selectedTab.png",
+			  data = {
+				filter = tabFilter
+			  }
+			}
+		
+		interactData.paneLayoutOverride["icon" .. tabName] = tabIconLabel
+		interactData.paneLayoutOverride["lbl" .. tabName] = tabNameLabel
+		table.insert(categories["buttons"], 1, tabButton)
+		posY = posY - 18
+	  end  
+	  interactData.paneLayoutOverride["categories"] = categories
+  end
+  
+  ---
+  --- End of testing
+  ---
 
   return { "OpenCraftingInterface", interactData }
 end
